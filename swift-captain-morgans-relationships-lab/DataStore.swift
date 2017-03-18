32c9bfebebe9e26f004b8bd75e52530df48525cb
edmund-h <447ght@gmail.com>
2017-03-18 15:37:53 -0400

    added random naming, ships detail view populates with correct ships now, can't get final segue to work

--- swift-captain-morgans-relationships-lab/DataStore.swift
+++ swift-captain-morgans-relationships-lab/DataStore.swift
@@ -21,6 +21,7 @@ final class DataStore {
     
     private init() {   }
     
+    // helper functions
     func getPiratesCount()->Int {
         
         return pirates.count
@@ -44,16 +45,19 @@ final class DataStore {
         return engines[index]
     }
     
+    func getShip(by name: String)-> Ship?{
+        return ships.first(where: {$0.name == name})
+    }//trist to find first ship with a given name
+    
+    //Test data setup
     func generateTestData(){
         
         let context = persistentContainer.viewContext
-        //let fetchReq: NSFetchRequest<Pirate> = Pirate.fetchRequest()
-        
         //20 pirates 10 ships per p
         print("In generateTestData")
         
         for count in 0..<20{
-            var name = "Pirate_\(count)"
+            var name = makePirateName()
             let pirate: Pirate = NSEntityDescription.insertNewObject(forEntityName: "Pirate", into: context) as! Pirate
             pirate.name = name
             pirates.append(pirate)
@@ -61,11 +65,11 @@ final class DataStore {
                 name = "Ship_\(count2)"
                 
                 let ship: Ship = NSEntityDescription.insertNewObject(forEntityName: "Ship", into: context) as! Ship
-                ship.name = name
+                ship.name = makeShipName()
                 ship.pirate = pirates[count]
                 
                 let engine: Engine = NSEntityDescription.insertNewObject(forEntityName: "Engine", into: context) as! Engine
-                let types = ["sail", "gas", "oars", "nuclear"]
+                let types = ["Sail", "Gas", "Oars", "Nuclear"]
                 engine.engineType = types[Int(arc4random_uniform(4))]
                 engines.append(engine)
                 ship.engine = engine
@@ -133,6 +137,31 @@ final class DataStore {
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
         }
+    }
+    
+    // MARK: Goofy Name Generators
+    func makePirateName()-> String{
+        let adjectives = ["Black","Blue","Red","Golden","Bloody","Drunken","Sea","Murder","Death","Jolly","Gray","Salty","Long"]
+        let nouns = [" Roger"," Jack"," Jim","beard","legs"," Edward"," PegLeg","scourge","bones","bane", " John"]
+        let titles = [" Silver"," the Cruel"," the Jolly", " the Admiral", " the Drunkard"," Scurvy", " Scallywag", " Ironsides", " the Buccanneer", " Morgan"]
+        
+        let adjective = adjectives[Int(arc4random_uniform(UInt32(adjectives.count)))]
+        let noun = nouns[Int(arc4random_uniform(UInt32(nouns.count)))]
+        let title = titles[Int(arc4random_uniform(UInt32(titles.count)))]
+        
+        return adjective + noun + title
+    }
+    
+    func makeShipName()-> String {
+        let adjectives = ["Black","Blue","Red","Golden","Bloody","Drunk","Sea","Murder","Death","Jolly","Gray","Salty","Long"]
+        let nouns = [" Raven", " Admiral", " Standard", " Kraken", " Leviathan", " Dragon", " Sunset", " Shark", " Cannon", " Herald", " Lady", " Sally", " Sails", " Decks"]
+        let descriptors = [" Good Ship ", " Stealthy ", " Fierce ", " Terrifying ", " Murderous ", " Ironsides ", " Veteran ", " Creaky ", " Swift ", " Captured "]
+        
+        let adjective = adjectives[Int(arc4random_uniform(UInt32(adjectives.count)))]
+        let noun = nouns[Int(arc4random_uniform(UInt32(nouns.count)))]
+        let descriptor = descriptors[Int(arc4random_uniform(UInt32(descriptors.count)))]
+        
+        return "The " + descriptor + adjective + noun
     }
 }
 
